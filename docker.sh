303844581c646f439257dfc0574812cf954a9888
xhb-gh <3096459788@qq.com>
2025-05-22 07:39:16 +0800

    Add files via upload

---
+++ docker.sh
@@ -0,0 +1,4 @@
+docker run -d  --name navidrome --restart=unless-stopped  \
+--user $(id -u):$(id -g)  -v /root/navidrome/music:/music   \
+-v /root/navidrome/data:/data  -p 4533:4533   \
+-e ND_LOGLEVEL=info  -e ND_BASEURL="/music"  deluan/navidrome:latest
